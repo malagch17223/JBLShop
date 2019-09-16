fb873eec0a0276cb9ecc26f82c8fd5ab12e330f5
malagch17223 <malagch17223@fpt.edu.vn>
2019-09-17 06:03:06 +0700

    Product Model and INMemory Product Repositiry Built

---
+++ JBL/JBLShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JBLShop.WebUI.MvcApplication" Language="C#" %>
---
+++ JBL/JBLShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JBLShop.WebUI.Startup))]
+namespace JBLShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
