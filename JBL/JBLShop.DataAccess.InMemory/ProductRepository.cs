e24228e6eab5a4a5f4fec3b7d965129b57d9a6a9
malagch17223 <malagch17223@fpt.edu.vn>
2019-09-17 06:59:54 +0700

    Product manager pages Built and tested

--- JBL/JBLShop.DataAccess.InMemory/ProductRepository.cs
+++ JBL/JBLShop.DataAccess.InMemory/ProductRepository.cs
@@ -16,59 +16,66 @@ namespace JBLShop.DataAccess.InMemory
         public ProductRepository()
         {
             products = cache["products"] as List<Product>;
-            if(products == null)
+            if (products == null)
             {
                 products = new List<Product>();
             }
-
         }
+
         public void Commit()
         {
             cache["products"] = products;
         }
+
         public void Insert(Product p)
         {
             products.Add(p);
         }
+
         public void Update(Product product)
         {
             Product productToUpdate = products.Find(p => p.Id == product.Id);
+
             if (productToUpdate != null)
             {
                 productToUpdate = product;
             }
             else
             {
-                throw new Exception("Product not found");
+                throw new Exception("Product no found");
             }
-
         }
+
         public Product Find(string Id)
         {
             Product product = products.Find(p => p.Id == Id);
+
             if (product != null)
             {
-                 return product;
+                return product;
             }
             else
             {
-                throw new Exception("Product not found");
+                throw new Exception("Product no found");
             }
         }
+
         public IQueryable<Product> Collection()
         {
             return products.AsQueryable();
         }
+
         public void Delete(string Id)
         {
             Product productToDelete = products.Find(p => p.Id == Id);
+
             if (productToDelete != null)
             {
                 products.Remove(productToDelete);
             }
             else
             {
-                throw new Exception("Product not found");
+                throw new Exception("Product no found");
             }
         }
     }
