2fb84d471ab9245a3483dc20e2714de5c755b59d
malagch17223 <malagch17223@fpt.edu.vn>
2019-09-17 08:02:15 +0700

    Product Grouping  Adding

--- JBL/JBLShop.WebUI/Controllers/ProductManagerController.cs
+++ JBL/JBLShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using JBLShop.core.Models;
+using JBLShop.core.ViewModels;
 using JBLShop.DataAccess.InMemory;
+
 
 namespace JBLShop.WebUI.Controllers
 {
+
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -25,8 +30,11 @@ namespace JBLShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -55,7 +63,11 @@ namespace JBLShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+
+                return View(viewModel);
             }
         }
 
---
+++ JBL/JBLShop.core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using JBLShop.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBLShop.core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
